5f3363e3e2d22e4c03ce1bcb260a8e5be10808fe
Damiano <belluccidamiano@gmail.com>
2021-01-08 02:18:03 +0100

    added feature preload metadata on media player

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,12 @@
 import React from "react";
-
 import "./App.css";
-import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
 
 function App() {
   return (
     <>
       <div className="App">
-        <audio controls preload="none">
+        <audio controls preload="metadata">
           <source
             src="http://localhost:2000?ciao=ciao"
             type="audio/mpeg"
